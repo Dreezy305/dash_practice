e589b275b269fdfaaa46a4affd0a48bed358d542
Dreezy <bankoleidris@gmail.com>
2021-12-30 20:36:07 +0100

    side bar

--- src/components /sidebar/Sidebar.js
+++ src/components /sidebar/Sidebar.js
@@ -22,9 +22,39 @@ function Sidebar() {
         </div>
 
         <div className="sidebarMenu">
-          <h3 className="sidebarTitle">Dashboard</h3>
+          <h3 className="sidebarTitle">Quick Menu</h3>
           <ul className="sidebarList">
             <li className="sidebarListItem">
+              <LineStyle className="sidebarIcon" /> Home
+            </li>
+            <li className="sidebarListItem">
+              <Timeline className="sidebarIcon" /> Analytics
+            </li>
+            <li className="sidebarListItem">
+              <TrendingUp className="sidebarIcon" /> Sales
+            </li>
+          </ul>
+        </div>
+
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Notifications</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem active">
+              <LineStyle className="sidebarIcon" /> Home
+            </li>
+            <li className="sidebarListItem">
+              <Timeline className="sidebarIcon" /> Analytics
+            </li>
+            <li className="sidebarListItem">
+              <TrendingUp className="sidebarIcon" /> Sales
+            </li>
+          </ul>
+        </div>
+
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Staff</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem active">
               <LineStyle className="sidebarIcon" /> Home
             </li>
             <li className="sidebarListItem">
