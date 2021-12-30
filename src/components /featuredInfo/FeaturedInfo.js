88ca3f498d0ec43dae960d1011d64e5ab78e091d
Dreezy <bankoleidris@gmail.com>
2021-12-30 21:40:04 +0100

    home page

--- src/components /featuredInfo/FeaturedInfo.js
+++ src/components /featuredInfo/FeaturedInfo.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./featuredInfo.css";
-import { ArrowDownward } from "@material-ui/icons";
+import { ArrowDownward, ArrowUpward } from "@material-ui/icons";
 
 function FeaturedInfo() {
   return (
@@ -11,6 +11,28 @@ function FeaturedInfo() {
           <span className="featuredMoney">$2,415</span>
           <span className="featuredMoneyRate">
             -11.4 <ArrowDownward />
+          </span>
+        </div>
+        <span className="featuredSub">Compared to Last Month</span>
+      </div>
+
+      <div className="featuredItem">
+        <span className="featuredTitle">Sales</span>
+        <div className="featuredMoneyContainer">
+          <span className="featuredMoney">$4,415</span>
+          <span className="featuredMoneyRate">
+            -1.4 <ArrowDownward />
+          </span>
+        </div>
+        <span className="featuredSub">Compared to Last Month</span>
+      </div>
+
+      <div className="featuredItem">
+        <span className="featuredTitle">Cost</span>
+        <div className="featuredMoneyContainer">
+          <span className="featuredMoney">$2,225</span>
+          <span className="featuredMoneyRate">
+            2.4 <ArrowUpward />
           </span>
         </div>
         <span className="featuredSub">Compared to Last Month</span>
