e853816a8907209ae03e35193d926546ec2921c5
Dreezy <bankoleidris@gmail.com>
2021-12-30 21:18:32 +0100

    side bar

--- src/components /featuredInfo/FeaturedInfo.js
+++ src/components /featuredInfo/FeaturedInfo.js
@@ -2,7 +2,16 @@ import React from "react";
 import "./featuredInfo.css";
 
 function FeaturedInfo() {
-  return <div>feature info</div>;
+  return (
+    <div className="featured">
+      <div className="featuredItem">
+        <span className="featuredTitle">Revenue</span>
+        <div className="featuredMoneyContainer">
+          <span className="featuredMoney">$2</span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default FeaturedInfo;
