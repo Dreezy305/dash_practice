06abd0471a9e81421ed46ff10f3e4742f7ee6dff
Dreezy <bankoleidris@gmail.com>
2021-12-30 10:52:31 +0100

    top bar

--- src/components /topbar/TopBar.js
+++ src/components /topbar/TopBar.js
@@ -5,7 +5,9 @@ function TopBar() {
   return (
     <div className="topbar">
       <div className="topbarWrapper">
-        <div className="topLeft">Left</div>
+        <div className="topLeft">
+          <span className="logo">HuntersWall</span>
+        </div>
         <div className="topRight">right</div>
       </div>
     </div>
