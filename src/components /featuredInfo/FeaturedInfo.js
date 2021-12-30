368959df1a29d0c76b5fec362420d6269b9127ac
Dreezy <bankoleidris@gmail.com>
2021-12-30 21:19:50 +0100

    side bar

--- src/components /featuredInfo/FeaturedInfo.js
+++ src/components /featuredInfo/FeaturedInfo.js
@@ -7,7 +7,8 @@ function FeaturedInfo() {
       <div className="featuredItem">
         <span className="featuredTitle">Revenue</span>
         <div className="featuredMoneyContainer">
-          <span className="featuredMoney">$2</span>
+          <span className="featuredMoney">$2,415</span>
+          <span className="featuredMoneyRate">-11.4</span>
         </div>
       </div>
     </div>
