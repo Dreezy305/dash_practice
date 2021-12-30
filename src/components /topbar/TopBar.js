1b81daaf58039f5f2ec1fadc37c864c556df8d34
Dreezy <bankoleidris@gmail.com>
2021-12-30 14:16:39 +0100

    top bar avatar icon

--- src/components /topbar/TopBar.js
+++ src/components /topbar/TopBar.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./topbar.css";
+import { NotificationsNone, Language, Settings } from "@material-ui/icons";
+import Avatar from "../../assets/avatar_two.svg";
 
 function TopBar() {
   return (
@@ -8,7 +10,20 @@ function TopBar() {
         <div className="topLeft">
           <span className="logo">HuntersWall</span>
         </div>
-        <div className="topRight">right</div>
+        <div className="topRight">
+          <div className="topbarIconContainer">
+            <NotificationsNone />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <Language />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <Settings />
+          </div>
+          <img src={Avatar} alt="profile_picture" className="topAvatar" />
+        </div>
       </div>
     </div>
   );
