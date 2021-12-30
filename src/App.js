62659333c2a584c62f07153ccc1a346e2efd598f
Dreezy <bankoleidris@gmail.com>
2021-12-30 19:20:08 +0100

    avatar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import TopBar from "./components /topbar/TopBar";
+import Sidebar from "./components /sidebar/Sidebar";
+import "./app.css";
 
 function App() {
   return (
     <div className="">
       <TopBar />
+      <div className="container">
+        <Sidebar />
+        <div className="others">other pages</div>
+      </div>
     </div>
   );
 }
---
+++ src/components /sidebar/Sidebar.js
@@ -0,0 +1,8 @@
+import "./sidebar.css";
+import React from "react";
+
+function Sidebar() {
+  return <div className="sidebar">Side bar</div>;
+}
+
+export default Sidebar;
