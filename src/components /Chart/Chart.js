6d0d9e337134efde567426e256c0d191e137bedf
Dreezy <bankoleidris@gmail.com>
2021-12-31 05:27:48 +0100

    chart component

--- src/components /Chart/Chart.js
+++ src/components /Chart/Chart.js
@@ -1,8 +1,92 @@
 import React from "react";
 import "./chart.css";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
 
 function Chart() {
-  return <div className="chart">charts</div>;
+  const data = [
+    {
+      name: "Jan",
+      "Active User": 4000,
+    },
+    {
+      name: "Feb",
+      "Active User": 3000,
+    },
+    {
+      name: "March",
+      "Active User": 2000,
+    },
+    {
+      name: "April",
+      "Active User": 2780,
+    },
+    {
+      name: "May",
+      "Active User": 1890,
+    },
+    {
+      name: "June",
+      "Active User": 2390,
+    },
+    {
+      name: "July",
+      "Active User": 3490,
+    },
+    {
+      name: "August",
+      "Active User": 3500,
+    },
+    {
+      name: "September",
+      "Active User": 4000,
+    },
+    {
+      name: "October",
+      "Active User": 4500,
+    },
+    {
+      name: "November",
+      "Active User": 5000,
+    },
+    {
+      name: "December",
+      "Active User": 5500,
+    },
+  ];
+
+  return (
+    <div className="chart">
+      <h3 className="chartTitle">User Analytics</h3>
+      <ResponsiveContainer width="100%" aspect={4 / 1}>
+        <LineChart
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="5 5" stroke="#e0dfdf" />
+          <XAxis dataKey="name" stroke="#5550bd" />
+
+          <Tooltip />
+          <Legend />
+
+          <Line type="monotone" dataKey="Active User" stroke="#82ca9d" />
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
+  );
 }
 
 export default Chart;
