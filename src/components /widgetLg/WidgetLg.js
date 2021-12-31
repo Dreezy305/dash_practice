7ef9b1eb7d636d96524fd0ea890be961f8bbe015
Dreezy <bankoleidris@gmail.com>
2021-12-31 09:19:27 +0100

    chart component

---
+++ src/components /widgetLg/WidgetLg.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./widgetLg.css";
+
+function WidgetLg() {
+  return <div>widget lg</div>;
+}
+
+export default WidgetLg;
---
+++ src/components /widgetSm/WidgetSm.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./widgetSm.css";
+
+function WidgetSm() {
+  return <div>widget</div>;
+}
+
+export default WidgetSm;
