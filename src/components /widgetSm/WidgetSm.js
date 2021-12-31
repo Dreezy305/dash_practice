1e9882c43e69c6983abe5b1ea7338237b96415a1
Dreezy <bankoleidris@gmail.com>
2021-12-31 09:39:22 +0100

    widget

--- src/components /widgetSm/WidgetSm.js
+++ src/components /widgetSm/WidgetSm.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./widgetSm.css";
 
 function WidgetSm() {
-  return <div className="widgetSm">widget</div>;
+  return (
+    <div className="widgetSm">
+      <span className="widgetSmTitle">New Members</span>
+    </div>
+  );
 }
 
 export default WidgetSm;
