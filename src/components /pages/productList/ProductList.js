09da5a690977ed4e96b1922b131e4db9265e35ec
Dreezy <bankoleidris@gmail.com>
2022-01-01 23:13:04 +0100

    edit user

--- src/components /pages/productList/ProductList.js
+++ src/components /pages/productList/ProductList.js
@@ -9,6 +9,70 @@ import { Link } from "react-router-dom";
 function ProductList() {
   const [data, setData] = useState(productRows);
 
+  const handleDelete = (id) => {
+    setData(data.filter((item) => item.id !== id));
+  };
+
+  const columns = [
+    { field: "id", headerName: "ID", width: 90 },
+    {
+      field: "product",
+      headerName: "Product",
+      width: 200,
+      editable: true,
+      renderCell: (params) => {
+        return (
+          <div className="productListUser" style={{}}>
+            <img
+              src={params.row.img}
+              className="productListImage"
+              alt="user_avatar"
+            />{" "}
+            {params.row.name}
+          </div>
+        );
+      },
+    },
+    {
+      field: "stock",
+      headerName: "Stock",
+      width: 200,
+    },
+    {
+      field: "status",
+      headerName: "Status",
+      width: 120,
+      editable: true,
+    },
+    {
+      field: "price",
+      headerName: "Price",
+      width: 160,
+      editable: true,
+    },
+    {
+      field: "action",
+      headerName: "Action",
+      width: 150,
+      renderCell: (params) => {
+        return (
+          <>
+            <Link to={"/product/" + params.row.id}>
+              <button className="productListEdit">Edit</button>
+            </Link>{" "}
+            <DeleteOutline
+              className="productListDelete"
+              onClick={() => {
+                handleDelete(params.row.id);
+                // setAlert(true);
+              }}
+            />
+          </>
+        );
+      },
+    },
+  ];
+
   return (
     <div className="productList">
       <DataGrid
