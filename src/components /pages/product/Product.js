cf25dafb730897463554779c77b5c9c5241a908f
Dreezy <bankoleidris@gmail.com>
2022-01-02 00:06:18 +0100

    products

--- src/components /pages/product/Product.js
+++ src/components /pages/product/Product.js
@@ -1,5 +1,8 @@
 import { Link } from "@material-ui/core";
 import React from "react";
+import Chart from "../../Chart/Chart";
+import { ProductData } from "../../../data";
+import Avatar from "../../../assets/avatar_two.svg";
 import "./product.css";
 
 function Product() {
@@ -12,8 +15,26 @@ function Product() {
         </Link>
       </div>
       <div className="productTop">
-        <div className="productTopLeft"></div>
-        <div className="productTopRight"></div>
+        <div className="productTopLeft">
+          <Chart
+            data={ProductData}
+            dataKey="Sales"
+            title="Sales Performance"
+            grid={true}
+          />
+        </div>
+        <div className="productTopRight">
+          <div className="productInfoTop">
+            <img src={Avatar} className="productInfoImg" alt="product_image" />
+            <span className="productName">Apple Airpods</span>
+          </div>
+          <div className="productInfoBottom">
+            <div className="productInfoItem">
+              <span className="productInfoKey">id:</span>
+              <span className="productInfoValue">Value</span>
+            </div>
+          </div>
+        </div>
       </div>
       <div className="productBottom"></div>
     </div>
