348b07e4f34dbfc93c401dad8450964358dd82b6
Dreezy <bankoleidris@gmail.com>
2022-01-01 21:56:49 +0100

    edit user

--- src/components /pages/NewUser/NewUser.js
+++ src/components /pages/NewUser/NewUser.js
@@ -4,7 +4,60 @@ import "./newUser.css";
 function NewUser() {
   return (
     <div className="newUser">
-      <div className="newUserTitle"></div>
+      <h1 className="newUserTitle">New User</h1>
+      <form className="newUserForm">
+        <div className="newUserItem">
+          <label>Username</label>
+          <input type="text" placeholder="john" />
+        </div>
+
+        <div className="newUserItem">
+          <label>Full Name</label>
+          <input type="text" placeholder="john smith" />
+        </div>
+
+        <div className="newUserItem">
+          <label>Email</label>
+          <input type="email" placeholder="john@gmail.com" />
+        </div>
+
+        <div className="newUserItem">
+          <label>Password</label>
+          <input type="password" placeholder="password..." />
+        </div>
+
+        <div className="newUserItem">
+          <label>Phone</label>
+          <input type="text" placeholder="+2347062383712" />
+        </div>
+
+        <div className="newUserItem">
+          <label>Address</label>
+          <input type="text" placeholder="Lagos 9ja" />
+        </div>
+
+        <div className="newUserItem">
+          <label>Gender</label>
+          <div className="newUserGender">
+            <input type="radio" name="gender" id="male" value="male" />
+            <label htmlFor="male">Male</label>
+
+            <input type="radio" name="gender" id="female" value="female" />
+            <label htmlFor="female">Female</label>
+
+            <input type="radio" name="gender" id="others" value="other" />
+            <label htmlFor="other">Other</label>
+          </div>
+        </div>
+
+        <div className="newUserItem">
+          <label>Active</label>
+          <select className="newUserSelect" name="active" id="active">
+            <option value="yes">Yes</option>
+            <option value="no">No</option>
+          </select>
+        </div>
+      </form>
     </div>
   );
 }
