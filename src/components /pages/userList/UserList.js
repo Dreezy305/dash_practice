9ee11393892e66d510b90c1110bd48272a5f115b
Dreezy <bankoleidris@gmail.com>
2022-01-01 01:20:43 +0100

    sidebar routing

--- src/components /pages/userList/UserList.js
+++ src/components /pages/userList/UserList.js
@@ -1,8 +1,10 @@
 import React from "react";
 import "./userList.css";
+import { DataGrid } from "@material-ui/data-grid";
 
 function UserList() {
   return <div className="userList">user List</div>;
 }
 
 export default UserList;
+// 1:24:47
