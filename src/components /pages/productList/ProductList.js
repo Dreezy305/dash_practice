dea9a2a7a62a17d3a2b861ee4e27ab1cdf32902c
Dreezy <bankoleidris@gmail.com>
2022-01-01 22:49:43 +0100

    edit user

--- src/components /pages/productList/ProductList.js
+++ src/components /pages/productList/ProductList.js
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./productList.css";
+import { DataGrid } from "@material-ui/data-grid";
+import { DeleteOutline } from "@material-ui/icons";
+import Avatar from "../../../assets/avatar_two.svg";
+import { productRows } from "../../../data";
+import { Link } from "react-router-dom";
 
 function ProductList() {
-  return <div className="productList">product list</div>;
+  const [data, setData] = useState(productRows);
+
+  return (
+    <div className="productList">
+      <DataGrid
+        rows={data}
+        columns={columns}
+        pageSize={10}
+        checkboxSelection
+        disableSelectionOnClick
+      />
+    </div>
+  );
 }
 
 export default ProductList;
